f3cbf9bb45548b70619f2afc013014cd4dd91eac
AilyGucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-07-06 00:23:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//write your code here
+var who = ["The cow", "The dog", "The banana", "My squirrel"];
+var how = [
+  "stole",
+  "decided to become a web developer",
+  " started telling jokes ",
+  "learned how to ride a bicycle"
+];
+var what = [
+  "my sandwich",
+  "during a quiet movie night",
+  "at a serious meeting",
+  "during a marathon"
+];
+var when = [
+  "during my lunch break",
+  "right at the end of the movie",
+  "right after the principal's speech",
+  "right in the middle of a cow competition"
+];
+var where = [
+  "at the park",
+  "on top of my laptop",
+  " in the classroom ",
+  "in the livestock arena"
+];
+function generateExcuse() {
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomHow = Math.floor(Math.random() * how.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  let randomWhere = Math.floor(Math.random() * where.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return (
+    who[randomWho] +
+    " " +
+    how[randomHow] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen] +
+    " " +
+    where[randomWhere]
+  );
+}
+/*window.onload = function() {
+  let excuse = generateExcuse();
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excuse;
+};*/
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("");
 };
